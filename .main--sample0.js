698e0a70fa0df0be4871ced55959134a7e0ed100
thdtjsdn <thdtjsdn@gmail.com>
2025-02-02 02:53:37 +0900

    U

--- .main--sample0.js
+++ .main--sample0.js
@@ -5,7 +5,14 @@ console.log(1);
 const CONFIG_MONGODB = {
 	ADMIN_ID: 'admin',
 	ADMIN_PW: '123qweasd',
-	PATH_SHELL: 'D:\\Development\\MongoDB_Shell\\2.3.8\\bin\\mongosh.exe'
+
+	//*/
+	PATH_SHELL: 'D:\\Development\\MongoDB_Shell\\2.3.8\\bin\\mongosh.exe',
+	PATH_QUERY: 'D:\\Development\\MongoDB_Query\\'
+	/*/
+	PATH_SHELL: '/Users/thdtjsdn/Development/MongoDB_Shell/2.3.8/bin/mongosh',
+	PATH_QUERY: '/Users/thdtjsdn/Development/MongoDB_Query/'
+	//*/
 };
 
 //----------------------------------------------------------------------------------------------------;
@@ -21,7 +28,7 @@ const CONFIG_MONGODB = {
 	UtilCLIDatabaseNoSQLMongoDB.executeQueryFile(
 		function (data) { AppLogger.log(`UtilCLIDatabaseNoSQLMongoDB.executeQueryFile : ${data}`); },
 		// function (data) { AppLogger.log(data, this); },
-		'D:\\Development\\MongoDB_Query\\db-test-countDocuments.dbjs',
+		CONFIG_MONGODB.PATH_QUERY + 'db-test-countDocuments.dbjs',
 		CONFIG_MONGODB.PATH_SHELL, CONFIG_MONGODB.ADMIN_ID, CONFIG_MONGODB.ADMIN_PW
 	);
 })();
@@ -37,7 +44,7 @@ const CONFIG_MONGODB = {
 	// AppLogger.log(data, this);
 
 	var result = UtilCLIDatabaseNoSQLMongoDB.syncExecuteQueryFile(
-		'D:\\Development\\MongoDB_Query\\db-test-countDocuments.dbjs',
+		CONFIG_MONGODB.PATH_QUERY + 'db-test-countDocuments.dbjs',
 		CONFIG_MONGODB.PATH_SHELL, CONFIG_MONGODB.ADMIN_ID, CONFIG_MONGODB.ADMIN_PW
 	);
 	AppLogger.log(`UtilCLIDatabaseNoSQLMongoDB.syncExecuteQueryFile : ${result}`, this);
